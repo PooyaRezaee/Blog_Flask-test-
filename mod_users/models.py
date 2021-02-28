f39e0288b6a8e64a56188bc5c15f6f34c4583470
Pooya <pooya.rezaee.official@gmail.com>
2021-02-28 15:29:49 +0330

    Change nullable in database

--- mod_users/models.py
+++ mod_users/models.py
@@ -3,11 +3,11 @@ from app import db
 from werkzeug.security import generate_password_hash
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'USERS'
 
     Id = db.Column(Integer(),primary_key=True)
-    full_name = db.Column(String(128),nullable=False)
-    Age = db.Column(Integer(),nullable=False)
+    full_name = db.Column(String(128),nullable=True)
+    Age = db.Column(Integer(),nullable=True)
     Email = db.Column(String(128),nullable=False,unique=True)
     password = db.Column(String(1280),nullable=False)
     rol = db.Column(String(128),nullable=False)
