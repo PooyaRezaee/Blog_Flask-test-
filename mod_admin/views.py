7b2a00c055b1d17f98242a2b16a6ceeca9dfe5bf
Pooya <pooya.rezaee.official@gmail.com>
2021-02-28 17:10:30 +0330

    add session

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,5 +1,12 @@
 from . import admin
+from flask import session
 
 @admin.route("/")
 def admin_index():
     return "welcom to panel admin"
+
+@admin.route("/")
+def login():
+    session['name'] = "pooya"
+
+    return "login page"
