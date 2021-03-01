55a24e13fe49c3c3333b7eddcb3e06eb2b75e9b2
Pooya <pooya.rezaee.official@gmail.com>
2021-03-01 16:49:16 +0330

    Created Forms login

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,5 +1,6 @@
 from . import admin
-from flask import session
+from flask import session,render_template,request
+from mod_users.forms import Loginform
 
 @admin.route("/")
 def admin_index():
@@ -7,6 +8,7 @@ def admin_index():
 
 @admin.route("/login/")
 def login():
-    session['name'] = "pooya"
+    # session['name'] = "pooya"
+    login_form = Loginform(request.form)
 
-    return "login page"
+    return render_template("admin/index.html",login_form=login_form)
