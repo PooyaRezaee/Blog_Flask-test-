b68bd3172ec33c49a02deb07b6b37bc887ebad7d
Pooya <pooya.rezaee.official@gmail.com>
2021-03-24 23:56:15 +0430

    show posts in blog

--- mod_blog/forms.py
+++ mod_blog/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 class Postform(FlaskForm):
     title = TextField(validators=[DataRequired()],render_kw={"placeholder":"title"})
-    summary = TextAreaField(validators=[DataRequired()],render_kw={"placeholder":"summary"})
+    summary = TextAreaField(render_kw={"placeholder":"summary"})
     content = TextAreaField(validators=[DataRequired()],render_kw={"placeholder":"content"})
     slug = TextField(validators=[DataRequired()],render_kw={"placeholder":"slug"})
--- mod_blog/models.py
+++ mod_blog/models.py
@@ -20,7 +20,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     title = Column(String(128), nullable=False, unique=True) #!
-    summary = Column(String(256), nullable=True, unique=False)
+    summary = Column(String(256), nullable=True)
     content = Column(Text, nullable=False, unique=False) #!
-    slug = Column(String(128), nullable=False, unique=True) #!
+    slug = Column(String(128), nullable=False, unique=True)#!
     categories = db.relationship('Category', secondary=posts_categories, back_populates='posts')
